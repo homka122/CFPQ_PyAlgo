b1ebaea623a06afb8036098f446942ee0758f515
homka122 <kombaev02@gmail.com>
2026-04-16 15:42:21 +0300

    Fix: don't add new assing term to graphs

--- cfpq_cli/run_all_pairs_cflr.py
+++ cfpq_cli/run_all_pairs_cflr.py
@@ -47,10 +47,14 @@ def convert_graph(num_contexts: int, graph_path: str) -> str:
                 if "open" in label:
                     context = int(label.split("_")[1]) % num_contexts
                     new_file.write(f"{frm}\t{to}\t({str(context)}\n")
+                    # new_file.write(f"{frm}\t{to}\tassign\n")
+                    # new_file.write(f"{to}\t{frm}\tassign_r\n")
                     # new_file.write(f"{to}\t{frm}\t){str(context)}\n")
                 elif "close" in label:
                     context = int(label.split("_")[1]) % num_contexts
                     new_file.write(f"{frm}\t{to}\t){str(context)}\n")
+                    # new_file.write(f"{frm}\t{to}\tassign\n")
+                    # new_file.write(f"{to}\t{frm}\tassign_r\n")
                     # new_file.write(f"{to}\t{frm}\t({str(context)}\n")
                 elif label in ["alloc", "assign"]:
                     new_file.write(f"{frm}\t{to}\t{label}\n")
